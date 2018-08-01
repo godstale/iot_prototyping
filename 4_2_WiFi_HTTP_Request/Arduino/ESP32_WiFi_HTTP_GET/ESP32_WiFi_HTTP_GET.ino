28b15ed5bce9cddc3607e45b1467505a19359fff
Young-Bae Suh <godstale@hotmail.com>
2018-08-02 00:49:52 +0900

    Update 4.2 examples

---
+++ 4_2_WiFi_HTTP_Request/Arduino/ESP32_WiFi_HTTP_GET/ESP32_WiFi_HTTP_GET.ino
@@ -0,0 +1,47 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+ 
+const char* ssid = "yourNetworkName";
+const char* password =  "yourPassword";
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+  delay(4000);
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+ 
+  Serial.println("Connected to the WiFi network");
+ 
+}
+ 
+void loop() {
+ 
+  if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
+ 
+    HTTPClient http;
+ 
+    http.begin("http://jsonplaceholder.typicode.com/comments?id=10"); //Specify the URL
+    int httpCode = http.GET();                                        //Make the request
+ 
+    if (httpCode > 0) { //Check for the returning code
+ 
+        String payload = http.getString();
+        Serial.println(httpCode);
+        Serial.println(payload);
+      }
+ 
+    else {
+      Serial.println("Error on HTTP request");
+    }
+ 
+    http.end(); //Free the resources
+  }
+ 
+  delay(10000);
+ 
+}
---
+++ 4_2_WiFi_HTTP_Request/Arduino/ESP32_WiFi_HTTP_POST/ESP32_WiFi_HTTP_POST.ino
@@ -0,0 +1,58 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+ 
+const char* ssid = "yourNetworkName";
+const char* password =  "yourNetworkPassword";
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+  delay(4000);   //Delay needed before calling the WiFi.begin
+ 
+  WiFi.begin(ssid, password); 
+ 
+  while (WiFi.status() != WL_CONNECTED) { //Check for the connection
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+ 
+  Serial.println("Connected to the WiFi network");
+ 
+}
+ 
+void loop() {
+ 
+ if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
+ 
+   HTTPClient http;   
+ 
+   http.begin("http://jsonplaceholder.typicode.com/posts");  //Specify destination for HTTP request
+   http.addHeader("Content-Type", "text/plain");             //Specify content-type header
+ 
+   int httpResponseCode = http.POST("POSTING from ESP32");   //Send the actual POST request
+ 
+   if(httpResponseCode>0){
+ 
+    String response = http.getString();                       //Get the response to the request
+ 
+    Serial.println(httpResponseCode);   //Print return code
+    Serial.println(response);           //Print request answer
+ 
+   }else{
+ 
+    Serial.print("Error on sending POST: ");
+    Serial.println(httpResponseCode);
+ 
+   }
+ 
+   http.end();  //Free resources
+ 
+ }else{
+ 
+    Serial.println("Error in WiFi connection");   
+ 
+ }
+ 
+  delay(10000);  //Send a request every 10 seconds
+ 
+}
